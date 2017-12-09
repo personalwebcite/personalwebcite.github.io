6b9963a09facc963fe7798a08a7110fa4883c122
mirkir <mole@p33.org>
2017-12-09 19:04:52 +0700

    gf

---
+++ js/script.js
@@ -0,0 +1,11 @@
+	$(document).ready(function() {
+		$('.info').particleground({
+				dotColor: '#777',
+				lineColor: '#777',
+				parallax: false,
+				lineWidth: 0.5,
+				particleRadius:5,
+				density: 7000
+		});
+	});
+
