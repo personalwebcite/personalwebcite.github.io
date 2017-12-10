6a3ae7b2963544384e0004f0bbb28a1c705d465a
personalwebcite <xirug@p33.org>
2017-12-10 21:39:55 +0700

    shift

--- js/script.js
+++ js/script.js
@@ -1,11 +1,47 @@
-	$(document).ready(function() {
-		$('.info').particleground({
-				dotColor: '#777',
-				lineColor: '#777',
-				parallax: false,
-				lineWidth: 0.5,
-				particleRadius:5,
-				density: 7000
-		});
-	});
+$( document ).ready(function(){
+// *********************************
+// ВИДЕО ФОН
+// *********************************
+        var myPlayer;
+        jQuery(function () {
+            var options = {
+                mobileFallbackImage: "https://avatanplus.com/files/resources/original/56d48a59c92b51532e3c6ee0.jpg",
+                playOnlyIfVisible  : false
+            };
+            myPlayer = jQuery(".player").YTPlayer(options);
+        });
+// *********************************
+// КЛИК ПО МИРОНЕНКО КИРИЛЛ
+// *********************************
+$("#link").dblclick(function(){ 
+	   $("#link").text('Shit the website'); 
+	  });
 
+// *********************************
+// СМЕНА ЦВЕТА
+// *********************************
+function ColorDiv(params) {
+        if (!(this instanceof ColorDiv)) {
+          return new ColorDiv(params);
+        }
+        this.initialize.apply(this, arguments);
+      }
+      ColorDiv.prototype.initialize = function (params) {
+        setInterval(function () {
+          var letters = '0123456789ABCDEF';
+          var color = '#';
+          for (var i = 0; i < 6; i++) {
+            color += letters.charAt(Math.floor(Math.random() * 16));
+          }
+          params.element.style.color = color;
+        }, params.interval);
+      }
+
+      ColorDiv({
+        element: document.getElementById('link'),
+        interval: 1500
+      });
+
+
+
+}); 
